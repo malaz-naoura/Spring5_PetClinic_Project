ae9ff72c80754d15cf759288b4293cb7c3fa28c5
malaz_naoura_rog <malaznaoura@gmail.com>
2023-12-04 10:58:55 +0300

    PET-10 Add id to all entity model

--- pet-clinic-data/src/main/java/com/mezo/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mezo/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.mezo.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private LocalDate birthDate;
     private PetType type;
     private Owner owner;
