1564c0b2a98cb8205f17cd95eebf7216f4652154
malaz_naoura <malaznaoura@gmail.com>
2023-12-14 14:42:17 +0300

    Change url for vet and owner to plural

--- pet-clinic-web/src/main/java/com/mezo/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mezo/petclinic/controller/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("owner")
+@RequestMapping("owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
