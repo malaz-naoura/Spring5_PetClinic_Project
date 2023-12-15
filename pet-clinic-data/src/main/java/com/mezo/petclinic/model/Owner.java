8386e529850b74cb971eb303d5af5a5bf9920998
malaz_naoura <malaznaoura@gmail.com>
2023-12-15 08:42:23 +0300

    PET-27 closeit, Create visit entity, add to owner set of pets relationship

--- pet-clinic-data/src/main/java/com/mezo/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/mezo/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.mezo.petclinic.model;
+
+import java.util.Set;
+
 public class Owner extends Person{
 //    private String address;
 //    private String city;
 //    private String telephone;
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
