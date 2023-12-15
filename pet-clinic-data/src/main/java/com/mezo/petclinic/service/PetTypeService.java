10f6c416810082c36a0eeecf1074db665c405bf1
malaz_naoura <malaznaoura@gmail.com>
2023-12-15 09:03:32 +0300

    PET-30 closeit, Add petType service and petType Map implementation

---
+++ pet-clinic-data/src/main/java/com/mezo/petclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.mezo.petclinic.service;
+
+import com.mezo.petclinic.model.PetType;
+import com.mezo.petclinic.service.CrudService;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
